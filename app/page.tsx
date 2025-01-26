17a49a33c1d7150832b156f433b63ca992bdd5b5
HeRa Khan <78750278+HOTLancerX@users.noreply.github.com>
2025-01-26 21:15:34 +0600

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from 'next/image';
+import Link from 'next/link';
 import React, { useState } from 'react';
 
 interface VideoItem {
@@ -40,14 +41,35 @@ function HomePage() {
   };
 
   return (
-    <div className="container mx-auto">
-      <div className="flex items-center mb-4">
+    <div className="container mx-auto px-2 md:px-6 py-10">
+      <div className='flex flex-col justify-center items-center space-y-4 text-white'>
+        <h1 className='md:text-8xl text-xl font-bold uppercase'>
+          Jumia
+        </h1>
+        <h2 className='md:text-4xl text-xl font-bold uppercase'>Similar script in Next.js</h2>
+        <p className='md:text-2xl text-xl font-bold uppercase'>install guideline</p>
+      </div>
+      <div className='flex flex-row justify-center mx-auto my-4 max-w-xl gap-4 items-center'>
+        <Link
+          href="https://jumias.vercel.app"
+          className='bg-white text-[] p-4 w-full rounded text-center font-bold'
+        >
+          DEMO
+        </Link>
+        <Link
+          href="https://jumias.vercel.app/admin"
+          className='bg-white text-[] p-4 w-full rounded text-center font-bold'
+        >
+          ADMIN
+        </Link>
+      </div>
+      <div className='my-4 md:my-10 sticky top-0'>
         <input
           type="text"
           placeholder="Search videos"
           value={searchTerm}
           onChange={handleSearch}
-          className="ml-2 border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+          className="p-2 md:p-4 w-full outline-0 rounded"
         />
       </div>
 
@@ -57,7 +79,7 @@ function HomePage() {
         ).map((item) => (
           <div
             key={item.id}
-            className="bg-white cursor-pointer"
+            className="bg-white hover:shadow-xl overflow-hidden rounded-md cursor-pointer"
             onClick={() => handleVideoClick(item.id)}
           >
             <Image
@@ -67,7 +89,7 @@ function HomePage() {
               alt={item.text}
               className=''
             />
-            <p>{item.text}</p>
+            <p className='p-2 leading-none text-base line-clamp-2'>{item.text}</p>
           </div>
         ))}
       </div>
